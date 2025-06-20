46224cd758164f8e19bed79da32c3ce32a8c1242
wtc <wtc@pop-os.localdomain>
2025-06-20 12:23:04 +0200

    adding sound

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,13 +1,40 @@
+let userClickedPattern = [];
 let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 let randomChoice = nextSequence();
 let randomChosenColour = buttonColours[randomChoice];
 gamePattern.push(randomChosenColour);
+let idSelect = "#" + randomChosenColour;
 
+
+let blink = jQuery("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+// alert(idSelect)
+// alert(randomChosenColour)
+// alert(selectButton)
+
+function playAudio(name) {
+    let soundUrl = name + ".mp3";
+    // alert(soundUrl);
+    alert(name)
+    let audio = new Audio("./sounds/"+soundUrl);
+    audio.play();
+    blink.attr("onclick", "playAudio()")
+}
+playAudio()
+
+
+jQuery(".btn").click(function () {
+    let userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+
+    
+})
 
 
 
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     console.log(randomNumber);
+    return randomNumber;
 }
