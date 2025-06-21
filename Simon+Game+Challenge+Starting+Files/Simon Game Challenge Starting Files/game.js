68aba960c5a9f33f31e25b8b6ab4a5c1c7b6f0f0
wtc <wtc@pop-os.localdomain>
2025-06-21 13:10:54 +0200

    code wasn't working 'cause i misspelled 'length' nxn

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -2,10 +2,53 @@ let userClickedPattern = [];
 let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 let level = 0
+let start = false;
 
-// alert(idSelect)
-// alert(randomChosenColour)
-// alert(selectButton)
+
+
+jQuery(document).on("keydown", function(o) {
+    if (!start) {
+        nextSequence();
+        start = true;
+    }
+    
+})
+
+
+jQuery(".btn").click(function () {
+    let lastElement = userClickedPattern.length - 1;
+    let userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+
+    console.log(userClickedPattern);
+    console.log(gamePattern);
+
+    animatePress(userChosenColour);
+    checkAnswer(lastElement);
+
+    playAudio(userChosenColour);
+})
+
+
+function nextSequence() {
+    userClickedPattern = [];
+    level += 1;
+
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    let idSelect = "#" + randomChosenColour;
+    jQuery("h1").text("Level "+ level)
+    
+
+
+    jQuery("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playAudio(randomChosenColour);
+
+}
+
 
 function playAudio(name) {
     let soundUrl = name + ".mp3";
@@ -15,37 +58,6 @@ function playAudio(name) {
     audio.play();
 }
 
-
-
-jQuery(".btn").click(function () {
-    let lastElement = userClickedPattern.length - 1;
-    let userChosenColour = this.id;
-    userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
-    checkAnswer(lastElement);
-    animatePress(userChosenColour);
-
-    playAudio(this.id)
-})
-
-
-function nextSequence() {
-    let randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber);
-    let randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
-    let idSelect = "#" + randomChosenColour;
-    jQuery("h1").text("Level "+ level)
-    level += 1;
-
-
-    jQuery("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    playAudio(randomChosenColour)
-}
-
-
-
 function animatePress(currentColour) {
     let elementID = "#" + currentColour;
     let self = jQuery(elementID);
@@ -54,25 +66,18 @@ function animatePress(currentColour) {
         self.removeClass("pressed");
     }, 100);
 }
-let start = false;
-jQuery(document).on("keydown", function(o) {
-    if (!start) {
-        nextSequence();
-    }
-    start = true;
-})
+
+
 
 
 function checkAnswer(currentLevel) {
-    if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
         console.log("Success");
-        for (let i = 0;  i < currentLevel; i++) {
-            if (userClickedPattern[i] == gamePattern[i]) {
-                console.log("Success in");
-            }
-            else {
-                console.log("noooooooo!!")
-            }
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000)
         }
     } else {
         console.log("wrong");
