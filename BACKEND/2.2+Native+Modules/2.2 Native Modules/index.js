ea9a02479cc7b834271d30c07bd9cfc39a0c1316
Akuthi <akuthiapril99@gmail.com>
2025-07-08 00:02:10 +0200

    first step into the backend world

---
+++ BACKEND/2.2+Native+Modules/2.2 Native Modules/index.js
@@ -0,0 +1,13 @@
+// import { readFile } from 'node:fs';
+
+const fs = require("fs");
+
+// fs.writeFile("message.txt", "Hello NodeJS!!", (err) => {
+//     if (err) throw err;
+//     console.log("The file has been saved!")
+// });
+
+fs.readFile('./message.txt', "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
