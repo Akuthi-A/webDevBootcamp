79e413fba7c4e5592248065db10e7cb41ffd4195
Akuthi <akuthiapril99@gmail.com>
2026-05-06 03:02:32 +0200

    react component exercise

---
+++ REACT/286-react-components-start/286-react-components/src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+   return <div>
+   <ul>
+      <li>stew</li>
+      <li>mogodu</li>
+      <li>pepper steak pie</li>
+    </ul>
+    </div>
+}
+
+export default List;
---
+++ REACT/286-react-components-start/286-react-components/src/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+
+
+ReactDOM.render(
+    <App />
+  ,
+  document.getElementById("root")
+);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//     <div>
+//       <Heading />
+//       <List />
+//     </div>
+// );
