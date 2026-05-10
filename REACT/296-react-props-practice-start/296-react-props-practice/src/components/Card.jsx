bd123746780fde3270094e9dfea69bb7c6a4b048
Akuthi <akuthiapril99@gmail.com>
2026-05-10 20:53:44 +0200

    using react devtools to debug

--- REACT/296-react-props-practice-start/296-react-props-practice/src/components/Card.jsx
+++ REACT/296-react-props-practice-start/296-react-props-practice/src/components/Card.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-
+import Avatar from "./Avatar";
+import Details from "./Details";
 
 function Card(props) {
   return <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img"
-            src={props.img}
-            alt="avatar_img"
-          />
+          <Avatar img={props.img}/>
         </div>
         <div className="bottom">
-            <div className="info">
+            {/* <div className="info">
                 <p>{props.tel}</p>
                 <p>{props.email}</p>
-            </div>
+            </div> */}
+            <Details detailInfo={props.tel}/>
+            <Details detailInfo={props.email}/>
           
         </div>
       </div>
