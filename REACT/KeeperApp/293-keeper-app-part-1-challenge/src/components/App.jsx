014ef1fb83cdce4a88562fa46922de5a859174a0
Akuthi <akuthiapril99@gmail.com>
2026-05-13 08:41:33 +0200

    Keeper app p2

--- REACT/KeeperApp/293-keeper-app-part-1-challenge/src/components/App.jsx
+++ REACT/KeeperApp/293-keeper-app-part-1-challenge/src/components/App.jsx
@@ -5,22 +5,20 @@ import Note from "./Note";
 import notes from "../notes";
 
 
-function createNote(note) {
-    return <Note 
-        key={note.key}
-        title={note.title}
-        content={note.content}
-        />
-    
-}
+
 
 //2. Create a App.jsx component.
 function App(props) {
     return <div>
         <Header />
 
-        {notes.map(createNote)}
-        {/* <Note /> */}
+        {notes.map(note => {
+            return <Note 
+        key={note.key}
+        title={note.title}
+        content={note.content}
+        />
+        })}
         
         <Footer />
     </div>
