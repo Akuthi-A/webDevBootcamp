ed8e7c1451169d6393a684dd9ec8ad94653d1099
Akuthi <akuthiapril99@gmail.com>
2026-05-14 23:25:46 +0200

    started with ternary operator in react

---
+++ REACT/303-conditional-rendering-start/303-conditional-rendering/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Input from "./Input";
+import Login from "./Login";
+
+
+var isLoggedIn = false;
+
+function App(props) {
+  return (
+    <div className="container">
+
+      {isLoggedIn? <h1>Hello</h1> : <Login />}
+          
+    </div>
+  );
+}
+
+export default App;
