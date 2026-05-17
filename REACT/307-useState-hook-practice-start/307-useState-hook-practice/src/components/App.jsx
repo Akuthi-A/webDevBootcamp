66cdb1c17013d2cbc27149cf63801e4c79f067d2
Akuthi <akuthiapril99@gmail.com>
2026-05-17 13:04:46 +0200

    useState practice4

--- REACT/307-useState-hook-practice-start/307-useState-hook-practice/src/components/App.jsx
+++ REACT/307-useState-hook-practice-start/307-useState-hook-practice/src/components/App.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
 function App() {
+  //Challenge:
+//1. Given that you can get the current time using:
+  let time = new Date().toLocaleTimeString();
+  console.log(time);
+
+//Show the latest time in the <h1> when the Get Time button
+//is pressed.
+  const [currentTime, setTime] = React.useState(time);
+  
+  function showTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  //2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+  setInterval(showTime, 1000)
+
+  
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTime}</h1>
+      <button onClick={showTime}>Get Time</button>
     </div>
   );
 }
